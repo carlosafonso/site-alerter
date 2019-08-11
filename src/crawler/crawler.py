991ea5aaec92e5125273004b47c31e812c5b447f
Carlos Afonso Pérez <carlos.afonso.perez@gmail.com>
2019-08-11 11:40:30 +0200

    Decouple scheduling of crawlers from actual crawler execution

--- src/crawler/crawler.py
+++ src/crawler/crawler.py
@@ -1,8 +1,11 @@
+import boto3
 import hashlib
+import json
 import os
 from repo import UrlRepository
 import requests
 
+sqs = boto3.client('sqs', endpoint_url=os.environ['SQS_ENDPOINT'])
 url_repo = UrlRepository(os.environ['URL_TABLE_NAME'], os.environ['DYNAMODB_ENDPOINT'])
 
 
@@ -10,9 +13,12 @@ class UrlCrawler(object):
     def __init__(self, url_repo):
         self.url_repo = url_repo
 
-    def crawl_urls(self):
-        urls = url_repo.get_urls()
-        for url in urls:
+    def crawl_url(self, id):
+        url = url_repo.find_url(id)
+
+        if url is None:
+            print("URL with ID {} does not exist".format(id))
+        else:
             print("Checking URL '{}'".format(url['url']))
             r = requests.get(url['url'])
             current_hash = hashlib.sha256(r.text.encode(r.encoding)).hexdigest()
@@ -25,6 +31,20 @@ class UrlCrawler(object):
                 print("Hashes match, no changes detected")
 
 
-def lambda_handler(event, context):
+def crawler_handler(event, context):
+    print("Payload has {} records".format(len(event['Records'])))
     crawler = UrlCrawler(url_repo)
-    crawler.crawl_urls()
+    for record in event['Records']:
+        body = json.loads(record['body'])
+        crawler.crawl_url(body['pk'])
+
+
+def scheduled_event_handler(event, context):
+    urls = url_repo.get_urls()
+    for url in urls:
+        print("Enqueuing {}".format(url['url']))
+        body = {'pk': url['pk']}
+        sqs.send_message(
+            QueueUrl=os.environ['CRAWLER_QUEUE_URL'],
+            MessageBody=json.dumps(body)
+        )
